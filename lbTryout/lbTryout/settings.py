7099264e517b700c499c3b691aed9cd844bcf435
ValeriaGamez <vgamez@usc.edu>
2024-05-31 15:41:04 -0700

    set debug to False

--- lbTryout/lbTryout/settings.py
+++ lbTryout/lbTryout/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c(x&n^j8g#dw*#2wa^b@%t$tia*tdx)s+p5vo78!2-ya@)xqxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lbtryout.onrender.com', 'lbtryout-1.onrender.com', '127.0.0.1']
 
